8cb3b7d9c0ebd52b64af2f214f465be8d4b21e89
ferhatgec <ferhatgectao@gmail.com>
2020-09-02 05:42:11 +0300

    Add Debug & Build & Run in argument parser.

--- flascript/FlaScriptMain.cpp
+++ flascript/FlaScriptMain.cpp
@@ -19,9 +19,10 @@
 
 void HelpFunction() {
 	std::cout << "Fegeya FlaScript\n" << 
-	"flascript --b || --build  <source> : Build & Run FlaScript code\n" <<
-	"flascript --d || --debug <source> : Debug FlaScript code\n" <<
-	"flascript --v || --version : Show version\n";
+	"flascript --b  || --build  <source> : Build & Run FlaScript code\n" <<
+	"flascript --d  || --debug <source> : Debug FlaScript code\n" <<
+	"flascript --ba || --buildall <source> : [Under the construction] Debug & Build & Run\n" <<
+	"flascript --v  || --version : Show version\n";
 }
 
 int main(int argc, char** argv) {
@@ -44,6 +45,11 @@ int main(int argc, char** argv) {
 		} else if(reg == "--debug" || reg == "--d") {
 			Debug_FInterpreter interp;
 			interp.Debug_FlaScriptInterpreter(copy_arg);
+		} else if(reg == "--buildall" || reg == "--ba") { /* Under the construction */
+			Debug_FInterpreter debug; /* Debug */
+			FInterpreter interp; /* Build & Run */
+			debug.Debug_FlaScriptInterpreter(copy_arg);
+			interp.FlaScriptInterpreter(copy_arg);
 		} else if(reg == "--help" || reg == "--h") {
 			HelpFunction();
 			exit(EXIT_SUCCESS);
